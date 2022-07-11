jonas/tig

implement user-programmable registers
 * %(register:r) expansion variable
 * set-register r value
 * !=(r)echo arbitrary shell command

src/status.c
@@ -676,7 +676,7 @@ open_mergetool(const char *file)
 {
 	const char *mergetool_argv[] = { "git", "mergetool", file, NULL };
 
-	open_external_viewer(mergetool_argv, repo.cdup, false, true, false, true, true, "");
+	open_external_viewer(mergetool_argv, repo.cdup, false, true, false, true, 0, true, "");
 }
 
 static enum request

x48Jason/itig

src/status.c
@@ -705,7 +705,7 @@ open_mergetool(const char *file)
 {
 	const char *mergetool_argv[] = { "git", "mergetool", file, NULL };
 
-	open_external_viewer(mergetool_argv, repo.exec_dir, false, true, false, true, true, "");
+	open_external_viewer(mergetool_argv, repo.exec_dir, false, true, false, true, 0, true, "");
 }
 
 static enum request